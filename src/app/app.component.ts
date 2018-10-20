920f4c199f8266a7a87a9468e580927857e834ed
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-20 15:03:20 +0300

    use loggedIn flag to decide when to show the login and logout links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 import { Logout } from './auth/auth.actions';
 import { AppState } from './reducers';
 import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
     constructor(private store: Store<AppState>) {
 
@@ -18,18 +21,22 @@ export class AppComponent implements OnInit {
 
     ngOnInit() {
 
-      this.store
-      .pipe(
-        map(state => state.auth.loggedIn)
-      )
-      .subscribe(loggedIn => console.log("loggedIn", loggedIn))
+        this.isLoggedIn$ = this.store
+            .pipe(
+                map(state => state.auth.loggedIn)
+            )
+
+        this.isLoggedOut$ = this.store
+            .pipe(
+                map(state => !state.auth.loggedIn)
+            )
 
     }
 
     logout() {
 
-      this.store.dispatch(new Logout());
-      
+        this.store.dispatch(new Logout());
+
     }
 
 
