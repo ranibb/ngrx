8efe7e8264930e088aa424df6842d0dc21507d79
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-21 15:11:49 +0300

    Little improvement: Redirect the user to login page after clicking logout link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Logout } from './auth/auth.actions';
 import { AppState } from './reducers';
 import { map } from 'rxjs/operators';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     isLoggedIn$: Observable<boolean>;
     isLoggedOut$: Observable<boolean>;
 
-    constructor(private store: Store<AppState>) {
+    constructor(private store: Store<AppState>, private router: Router) {
 
     }
 
@@ -37,6 +38,8 @@ export class AppComponent implements OnInit {
     logout() {
 
         this.store.dispatch(new Logout());
+
+        this.router.navigateByUrl('/login');
 
     }
 
