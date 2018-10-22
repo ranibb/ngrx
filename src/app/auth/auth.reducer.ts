ce875bf52e8f389e6e8a958c1e4f86a5dc574ca1
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-22 18:14:12 +0300

    Making sure that our components will not directly mutate the state inside the store (which would break the store architecture) by the help of ngrx-store-freeze.

--- src/app/auth/auth.reducer.ts
+++ src/app/auth/auth.reducer.ts
@@ -23,6 +23,12 @@ export function authReducer(state = initialAuthState, action: AuthActions): Auth
       user: action.payload.user
     }
 
+    // Returning an object that mutates the store initial state will be prevented by storeFreeze 
+    // case AuthActionTypes.LogoutAction:
+    //   state.loggedIn = false;
+    //   state.user = undefined;
+    //   return state;
+
     case AuthActionTypes.LogoutAction: return {
       loggedIn: false,
       user: undefined
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,5 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../environments/environment';
 import { AuthState, authReducer } from '../auth/auth.reducer';
 
@@ -10,4 +11,10 @@ export const reducers: ActionReducerMap<AppState> = {
   auth: authReducer
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+/**
+ * Enabling storeFreeze in metaReducers in the production mode. metaReducers are applied after 
+ * all our application reducers have been executed. Enabling storeFreeze here will take the 
+ * application state and recursively freezes all properties of each javascript object by making 
+ * them read-only.
+ */
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
